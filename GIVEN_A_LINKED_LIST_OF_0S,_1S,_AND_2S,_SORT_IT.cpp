757316a15415fbb2154cb44874f38f4876fdb3c2
AndrewSiwi <67230305+AndrewSiwi@users.noreply.github.com>
2023-07-23 21:24:09 +0200

    Update GIVEN_A_LINKED_LIST_OF_0S,_1S,_AND_2S,_SORT_IT.cpp

--- GIVEN_A_LINKED_LIST_OF_0S,_1S,_AND_2S,_SORT_IT.cpp
+++ GIVEN_A_LINKED_LIST_OF_0S,_1S,_AND_2S,_SORT_IT.cpp
@@ -28,7 +28,28 @@ class Solution
 
   void process()
   {
-    
+    vector<int> vals;
+        
+        for(int i = 0; i < 3; i++)
+        {
+            Node* current = head;
+            while(current != NULL)
+            {
+                if(current->data == i) vals.push_back(current->data);
+                current = current->next;
+            }
+        }
+        
+        Node* start = new Node(vals[0]);
+        Node* current_end = start;
+        
+        for(int i = 1; i < vals.size(); i++)
+        {
+            current_end->next = new Node(vals[i]);
+            current_end = current_end->next;
+        }
+        
+        return start;
   }
 
   void print()
