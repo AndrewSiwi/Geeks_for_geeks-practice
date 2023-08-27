3c93a495765d95320e77549245281a1ab7deda1d
AndrewSiwi <67230305+AndrewSiwi@users.noreply.github.com>
2023-08-27 10:07:55 +0200

    create problem
    SECOND_LARGEST

---
+++ SECOND_LARGEST/SECOND_LARGEST.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define STEROID_NUMBER long long
+
+#define ITER(var, start, cond, end, diff) for(int var = (start); var cond (end); var += diff)
+#define ITER_FIX(var, start, end) ITER(var, start, <, end, 1)
+#define ITER_ARR(var) ITER_FIX(var, 0, this->n)
+#define ITER_REVERSE(var, start, end) ITER(var, start, >=, end, -1)
+
+#define ITER_CONT(var, cont) for(auto& var: cont)
+#define ITER_CONT_IT(var, cont) for(auto var = cont.begin(); var != cont.end();)
+
+#define N 100000
+
+class Solution
+{
+    static int t;
+    public: static void init()
+    {
+        //cin >> t;
+        int ct = 0;
+        while(ct < t /*!cin.eof()*/)
+        {
+            Solution* sl = new Solution(ct++);
+            sl->process();
+            sl->print();
+            delete sl;
+        }
+    }
+
+
+    private: int ct;
+
+    int n, arr[N], ret = -1;
+
+    public: Solution(int ct)
+    {
+        this->ct = ct;
+
+        cin >> this->n;
+        int n = this->n;
+        while(n--)
+        {
+            int x;
+            cin >> x;
+            this->arr[this->n - n - 1] = x;
+        }
+    }
+
+    void process()
+    {
+        int largest = this->arr[0];
+        ITER_ARR(i) if(this->arr[i] > largest) largest = this->arr[i];
+        ITER_ARR(i) if(this->arr[i] < largest && this->arr[i] > this->ret) this->ret = this->arr[i];
+    }
+
+    void print()
+    {
+        cout << this->ret << "\n";
+        //cout << "\n";
+    }
+};
+
+int Solution::t = 1;
+
+int main(int argc, char* argv[])
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Solution::init();
+
+    return 0;
+}
